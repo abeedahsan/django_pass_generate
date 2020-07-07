29a30b9ee904f79ad455d0aa1023ee0be547ed36
Abeed Ahsan Khan Akash <u1504057@student.student.cuet.ac.bd>
2020-07-07 20:48:36 +0600

    secont commit

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cap5gzhwcfn=x^5(&mkrv-(wo1ukhy7caxej#den40cawgt3yy'
+SECRET_KEY = 'akacap5gzhwcfn=x^5(&mkrv-(wo1ukhy7caxej#den40cawgt3yy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
